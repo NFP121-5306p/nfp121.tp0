62ff1b4a597742b7e6d9a7314eef54f967a23c9f
elias <elias.semaan@isae.edu.lb>
2021-11-28 20:58:41 +0200

    Tp0

---
+++ blueJ-Tp0/type.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class type here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class type
+{
+    public static void main (String[] args) { 
+        car ford= new car();
+     } 
+}
